c875a01b1fc8115eff813cb5ca0edf3d637d65c5
Mewpop <79798505+Mewpop@users.noreply.github.com>
2021-04-09 09:46:11 +0530

    sadawd

---
+++ index.js
@@ -0,0 +1,49 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+const embed = new Discord.MessageEmbed()
+.setTitle('HELP')
+.addField('%hello')
+.addField("%info")
+.addField("%how much will i score in my exams ?")
+.addField("%how much time will i fail in exams ?")
+.addField('%support')
+.addField("%profile")
+.addField('%how is KTG ?')
+.addField('%in which standard KTG is studying ?')
+.addField('%which game do KTG play ?')
+.addField('%where does KTG live ?')
+.addField('%feeling bad')
+.addField('%order food')
+.addField('%KTG ke dost')
+.addField('%report a problem')
+.addField('%act')
+.setColor('RANDOM')
+
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+  client.user.setPresence({ activity: { name: "FREE FIRE" } });
+
+});
+
+client.on('message', msg => {
+  if (msg.content === '%help') {
+    msg.channel.send(embed)
+  }
+  
+        
+  
+
+  
+  
+  
+
+  
+
+
+});
+
+
+
+client.login(process.env.token);
